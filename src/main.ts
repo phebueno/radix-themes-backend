0a1cde9dcf2691b4d4ec772d97186dbce59727c4
Fernando Bueno <fcbgomes@gmail.com>
2024-10-01 01:36:21 -0300

    feats: added new main.ts configs - activated dto validation via ValidationPipe - enabledCors for future api connections - added custom api port choice - created .env.example

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Server is up and runnning on port ${port}`);
+  });
 }
 bootstrap();
