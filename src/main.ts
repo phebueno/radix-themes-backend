fb5ac24ffccb28ecc3c1f196b1e04de76d35e4cb
Fernando Bueno <fcbgomes@gmail.com>
2024-10-04 03:07:49 -0300

    WIP: started swagger API documentation. changed default route to getHealth route

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('RadixThemes - REST API Documentation')
+    .setDescription(
+      `This API provides a set of services for managing themes
+      and their associated links, enabling the creation, listing, 
+      updating, and deletion of themes, as well as associating relevant
+      links with them. It supports pagination in its routes, allowing
+      efficient navigation of large datasets.`,
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
